0457cec3525732b94fc87184a41060c7760f3408
izacpack <pachis00@CMD15GRCC.cernlzmpa.com>
2015-10-13 21:03:49 -0400

    Removed imports

--- twotofive-video1/src/CubeDemo.java
+++ twotofive-video1/src/CubeDemo.java
@@ -1,89 +1,50 @@
-// Left click tiles to move.  Right click to flip.  Have fun!
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.BiFunction;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
-import javax.swing.GroupLayout.SequentialGroup;
-
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
-import javafx.animation.ParallelTransition;
-import javafx.animation.SequentialTransition;
-import javafx.animation.TimelineBuilder;
-import javafx.application.Application;
-import javafx.geometry.Point3D;
-import javafx.geometry.Pos;
-import javafx.scene.Group;
-import javafx.scene.GroupBuilder;
-import javafx.scene.Node;
-import javafx.scene.PerspectiveCamera;
-import javafx.scene.Scene;
-import javafx.scene.SceneBuilder;
-import javafx.scene.image.Image;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.StackPaneBuilder;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.ImagePattern;
-import javafx.scene.shape.RectangleBuilder;
-import javafx.scene.transform.Rotate;
-import javafx.stage.Stage;
-import javafx.stage.StageBuilder;
-import javafx.util.Duration;
-import javafx.event.EventHandler;
-import javafx.scene.input.MouseButton;
-
 // Class declaration and two static imports = 3 statements - total = 3
-public class CubeDemo extends Application {
+public class CubeDemo extends javafx.application.Application {
 	private static int emptyPos = 8, tilePos[] = {0,1,2,3,4,5,6,7}, tileFaceUp[] = {1,1,1,1,1,1,1,1};
 
-	private static final List<Group> tiles = IntStream.range(0, 8).mapToObj(i -> GroupBuilder.create().<Group>children(Arrays.<Node>asList(new Node[] {
-	        RectangleBuilder.create() // back face
+	private static final java.util.List<javafx.scene.Group> tiles = java.util.stream.IntStream.range(0, 8).mapToObj(i -> javafx.scene.GroupBuilder.create().<javafx.scene.Group>children(java.util.Arrays.<javafx.scene.Node>asList(new javafx.scene.Node[] {
+			javafx.scene.shape.RectangleBuilder.create() // back face
 	        .width((double) 175).height((double) 175)
-	        .fill(new ImagePattern(new Image("file:media/firsthand-"+(i/3)+"-"+(i%3)+".png")))
+	        .fill(new javafx.scene.paint.ImagePattern(new javafx.scene.image.Image("file:media/firsthand-"+(i/3)+"-"+(i%3)+".png")))
 	        .translateX(-0.5 * (double) 175)
 	        .translateY(-0.5 * (double) 175)
 	        .translateZ(0.5 * (double) 20)
 	        .build(),
-	        RectangleBuilder.create() // bottom face
+	        javafx.scene.shape.RectangleBuilder.create() // bottom face
 	        .width((double) 175).height((double) 20)
-	        .fill(Color.DARKBLUE.deriveColor(0.0, 1.0, (1 - 0.4 * 1), 1.0))
+	        .fill(javafx.scene.paint.Color.DARKBLUE.deriveColor(0.0, 1.0, (1 - 0.4 * 1), 1.0))
 	        .translateX(-0.5 * (double) 175)
 	        .translateY(0.5 * ((double) 175 - (double) 20))
-	        .rotationAxis(Rotate.X_AXIS)
+	        .rotationAxis(javafx.scene.transform.Rotate.X_AXIS)
 	        .rotate(90)
 	        .build(),
-	        RectangleBuilder.create() // right face
+	        javafx.scene.shape.RectangleBuilder.create() // right face
 	        .width((double) 20).height((double) 175)
-	        .fill(Color.DARKBLUE.deriveColor(0.0, 1.0, (1 - 0.3 * 1), 1.0))
+	        .fill(javafx.scene.paint.Color.DARKBLUE.deriveColor(0.0, 1.0, (1 - 0.3 * 1), 1.0))
 	        .translateX(0.5 * ((double) 175-(double) 20))
 	        .translateY(-0.5 * (double) 175)
-	        .rotationAxis(Rotate.Y_AXIS)
+	        .rotationAxis(javafx.scene.transform.Rotate.Y_AXIS)
 	        .rotate(90)
 	        .build(),
-	        RectangleBuilder.create() // left face
+	        javafx.scene.shape.RectangleBuilder.create() // left face
 	        .width((double) 20).height((double) 175)
-	        .fill(Color.DARKBLUE.deriveColor(0.0, 1.0, (1 - 0.2 * 1), 1.0))
+	        .fill(javafx.scene.paint.Color.DARKBLUE.deriveColor(0.0, 1.0, (1 - 0.2 * 1), 1.0))
 	        .translateX(-0.5 * ((double) 175+(double) 20))
 	        .translateY(-0.5 * ((double) 175))
-	        .rotationAxis(Rotate.Y_AXIS)
+	        .rotationAxis(javafx.scene.transform.Rotate.Y_AXIS)
 	        .rotate(90)
 	        .build(),
-	        RectangleBuilder.create() // top face
+	        javafx.scene.shape.RectangleBuilder.create() // top face
 	        .width((double) 175).height((double) 20)
-	        .fill(Color.DARKBLUE.deriveColor(0.0, 1.0, (1 - 0.1 * 1), 1.0))
+	        .fill(javafx.scene.paint.Color.DARKBLUE.deriveColor(0.0, 1.0, (1 - 0.1 * 1), 1.0))
 	        .translateX(-0.5 * (double) 175)
 	        .translateY(-0.5 * ((double) 175+(double) 20))
-	        .rotationAxis(Rotate.X_AXIS)
+	        .rotationAxis(javafx.scene.transform.Rotate.X_AXIS)
 	        .rotate(90)
 	        .build(),
-	        RectangleBuilder.create() // front face
+	        javafx.scene.shape.RectangleBuilder.create() // front face
 	        .width((double) 175).height((double) 175)
-	        .fill(new ImagePattern(new Image("file:media/cerner-"+(i/3)+"-"+(i%3)+".png")))
+	        .fill(new javafx.scene.paint.ImagePattern(new javafx.scene.image.Image("file:media/cerner-"+(i/3)+"-"+(i%3)+".png")))
 	        .translateX(-0.5 * (double) 175)
 	        .translateY(-0.5 * (double) 175)
 	        .translateZ(-0.5 * (double) 20)
@@ -93,44 +54,44 @@ public class CubeDemo extends Application {
 	.translateX(i%3 * ((double) 175 + 2))
 	.translateY(i/3 * ((double) 175 + 2))
 	.build())
-	.collect(Collectors.toList());
+	.collect(java.util.stream.Collectors.toList());
 
-	// Start method being/end brackets and one statement of content = 3 statements - total = 6
-    public void start(Stage stage) {
-    	StageBuilder.create().title("Cerner and First Hand").scene(SceneBuilder.create().root(
-        		StackPaneBuilder.create()
+	// Start method begin/end brackets and one statement of content = 3 statements - total = 6
+    public void start(javafx.stage.Stage stage) {
+    	javafx.stage.StageBuilder.create().title("Cerner and First Hand").scene(javafx.scene.SceneBuilder.create().root(
+    			javafx.scene.layout.StackPaneBuilder.create()
     			.layoutX(20)
     			.layoutY(20)
-    			.alignment(Pos.TOP_LEFT)
+    			.alignment(javafx.geometry.Pos.TOP_LEFT)
     			.children(tiles)
     			.style("-fx-background-color: cyan")
     			.build()
-        		).camera(new PerspectiveCamera()).width(570).height(570).depthBuffer(true).build()).build().show();
+        		).camera(new javafx.scene.PerspectiveCamera()).width(570).height(570).depthBuffer(true).build()).build().show();
     }
     
-    // moveOrFlip method being/end brackets and 10 statements of content = 10 statements - total = 16
-    private static void moveOrFlipTile(int tileIndex, MouseEvent e) {
-		if (e.getButton() == MouseButton.PRIMARY) {
+    // moveOrFlip method begin/end brackets and 10 statements of content = 10 statements - total = 16
+    private static void moveOrFlipTile(int tileIndex, javafx.scene.input.MouseEvent e) {
+		if (e.getButton() == javafx.scene.input.MouseButton.PRIMARY) {
         	int targetPos = emptyPos;
-        	TimelineBuilder.create().keyFrames(
-			        new KeyFrame(Duration.ZERO,
-			            new KeyValue(tiles.get(tileIndex).translateXProperty(), tilePos[tileIndex]%3*177),
-			            new KeyValue(tiles.get(tileIndex).translateYProperty(), tilePos[tileIndex]/3*177)),
-			        new KeyFrame(Duration.seconds(1),
-			            new KeyValue(tiles.get(tileIndex).translateXProperty(), targetPos%3*177),
-			            new KeyValue(tiles.get(tileIndex).translateYProperty(), targetPos/3*177)))
+        	javafx.animation.TimelineBuilder.create().keyFrames(
+			        new javafx.animation.KeyFrame(javafx.util.Duration.ZERO,
+			            new javafx.animation.KeyValue(tiles.get(tileIndex).translateXProperty(), tilePos[tileIndex]%3*177),
+			            new javafx.animation.KeyValue(tiles.get(tileIndex).translateYProperty(), tilePos[tileIndex]/3*177)),
+			        new javafx.animation.KeyFrame(javafx.util.Duration.seconds(1),
+			            new javafx.animation.KeyValue(tiles.get(tileIndex).translateXProperty(), targetPos%3*177),
+			            new javafx.animation.KeyValue(tiles.get(tileIndex).translateYProperty(), targetPos/3*177)))
 			    .build().play();
             emptyPos = tilePos[tileIndex];
             tilePos[tileIndex] = targetPos;
     	} else {
     		tileFaceUp[tileIndex] = -1 * tileFaceUp[tileIndex];
-		    TimelineBuilder.create().keyFrames(
-				    new KeyFrame(Duration.ZERO,
-				        new KeyValue(tiles.get(tileIndex).rotationAxisProperty(), ((tileIndex/3-(tileIndex%3)) & 1) == 1 ? Rotate.X_AXIS : Rotate.Y_AXIS),
-				        new KeyValue(tiles.get(tileIndex).rotateProperty(), tileFaceUp[tileIndex] < 0 ? 0d : 180d)),
-				    new KeyFrame(Duration.seconds(1),
-				        new KeyValue(tiles.get(tileIndex).rotationAxisProperty(), ((tileIndex/3-(tileIndex%3)) & 1) == 1 ? Rotate.X_AXIS : Rotate.Y_AXIS),
-				        new KeyValue(tiles.get(tileIndex).rotateProperty(), tileFaceUp[tileIndex] < 0  ? 180d : 360d)))
+    		javafx.animation.TimelineBuilder.create().keyFrames(
+				    new javafx.animation.KeyFrame(javafx.util.Duration.ZERO,
+				        new javafx.animation.KeyValue(tiles.get(tileIndex).rotationAxisProperty(), ((tileIndex/3-(tileIndex%3)) & 1) == 1 ? javafx.scene.transform.Rotate.X_AXIS : javafx.scene.transform.Rotate.Y_AXIS),
+				        new javafx.animation.KeyValue(tiles.get(tileIndex).rotateProperty(), tileFaceUp[tileIndex] < 0 ? 0d : 180d)),
+				    new javafx.animation.KeyFrame(javafx.util.Duration.seconds(1),
+				        new javafx.animation.KeyValue(tiles.get(tileIndex).rotationAxisProperty(), ((tileIndex/3-(tileIndex%3)) & 1) == 1 ? javafx.scene.transform.Rotate.X_AXIS : javafx.scene.transform.Rotate.Y_AXIS),
+				        new javafx.animation.KeyValue(tiles.get(tileIndex).rotateProperty(), tileFaceUp[tileIndex] < 0  ? 180d : 360d)))
 				.build().play();
     	}
     }
